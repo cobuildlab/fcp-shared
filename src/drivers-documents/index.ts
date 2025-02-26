34db49f24a75fdcf1bf4f807b94a738f8513c0ec
perdomofranklindev <perdomofranklindev@gmail.com>
2025-02-26 15:10:28 -0400

    Add DriverDocuments enum to manage document types

---
+++ src/drivers-documents/index.ts
@@ -0,0 +1,7 @@
+export enum DriverDocuments {
+  DriverLicense = 'DRIVER_LICENSE',
+  EmploymentApplication = 'EMPLOYMENT_APPLICATION',
+  MvrReport = 'MVR_REPORT',
+  MedicalCertificate = 'MEDICAL_CERTIFICATE',
+  MedicalExaminerVerificationNote = 'MEDICAL_EXAMINER_VERIFICATION_NOTE',
+}

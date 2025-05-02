fc19ff1c5f808ad810feeb47bdf5107da4a6e6b8
perdomofranklindev <perdomofranklindev@gmail.com>
2025-05-02 16:42:33 -0400

    Update CommercialDriverLicense enum value to clarify change from DRIVER_LICENSE

--- src/drivers-documents/index.ts
+++ src/drivers-documents/index.ts
@@ -1,5 +1,5 @@
 export enum DriverDocumentType {
-  CommercialDriverLicense = 'COMMERCIAL_DRIVER_LICENSE',
+  CommercialDriverLicense = 'COMMERCIAL_DRIVER_LICENSE', // This was changed from 'DRIVER_LICENSE' to 'COMMERCIAL_DRIVER_LICENSE'
   EmploymentApplication = 'EMPLOYMENT_APPLICATION',
   MvrReport = 'MVR_REPORT',
   MedicalCertificate = 'MEDICAL_CERTIFICATE',
